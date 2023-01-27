ed0bd99983fdebf71e33e32353ce4650f565bc1e
Renan Andrade <85353999+renanandradebr@users.noreply.github.com>
2023-01-26 23:11:48 -0300

    commitando o projeto

---
+++ Projeto/script.js
@@ -0,0 +1,25 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+   
+
+    msg.innerHTML = `Agora são ${hora} horas. `
+    if(hora >= 0 && hora < 12){
+        // BOM DIA
+        img.src = 'fotomanha.png'
+        document.body.style.background = "#80825B"
+
+    } else if (hora >= 12 && hora < 18){
+        //BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = "#CF856F"
+
+    } else {
+        //BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = "#0E4585"
+
+    }
+}
